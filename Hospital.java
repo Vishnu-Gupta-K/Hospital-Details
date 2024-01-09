5fdf8c6f05bec349340d68463037b91bf853ee89
Vishnu-Gupta-K <156089347+Vishnu-Gupta-K@users.noreply.github.com>
2024-01-09 19:39:10 +0530

    Add files via upload
    Hello,
    My name is Vishnu Gupta and I would like to present my small project called Hospital. The purpose of this project is to create a simple application that can display the information of doctors and patients in a hospital. This project is not very complex, but it demonstrates some basic features of programming and data management. I hope you find it useful and informative. Thank you for your attention.

---
+++ Hospital.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Doctor 
+{
+public String dname,spe;
+public void showdoc()
+{
+System.out.println(" Doctor Name Is : "+dname);
+System.out.println(" Doctor Specialization Is : "+spe);
+}
+}
+
+class Patient
+{
+public String pname,problem;
+public void showpat()
+{
+System.out.println(" Patient Name Is : "+pname);
+System.out.println(" Patient Problem Is : "+problem);
+}
+}
+public class Hospital
+{
+public static void main(String arg[ ])
+{
+Scanner sc =new Scanner(System.in);
+Doctor d= new Doctor();
+Patient p= new Patient();
+System.out.println("Enter Doctor Name : ");
+d.dname=sc.nextLine();
+System.out.println("Enter Patient Name : ");
+p.pname=sc.nextLine();
+System.out.println("Enter Doctor Specialization : ");
+d.spe=sc.nextLine();
+System.out.println("Enter Patient Problem : ");
+p.problem=sc.nextLine();
+d.showdoc();
+p.showpat();
+}
+}
